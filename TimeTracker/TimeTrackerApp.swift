b32d5f78edca21f4542a72aca62bfdcf142bed5c
Martin Altmann <martin.altmann@gmail.com>
2024-09-17 15:53:06 +0200

    Initial Commit

---
+++ TimeTracker/TimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerApp.swift
+//  TimeTracker
+//
+//  Created by Martin Altmann on 17.09.24.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
