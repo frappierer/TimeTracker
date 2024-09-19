b481e609575a9d644bd801e4b289ebb29f4432d1
Martin Altmann <martin.altmann@gmail.com>
2024-09-19 11:14:02 +0200

    Initial

---
+++ TimeTracker/SettingsView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsView.swift
+//  TimeTracker
+//
+//  Created by Martin Altmann on 17.09.24.
+//
+
+import Foundation
---
+++ TimeTracker/TimeTracker.swift
@@ -0,0 +1,8 @@
+//
+//  TimeTracker.swift
+//  TimeTracker
+//
+//  Created by Martin Altmann on 17.09.24.
+//
+
+import Foundation
